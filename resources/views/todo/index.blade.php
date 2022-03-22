79e483d1790e4d298d077f5c4b0541eb6b264203
FSA Dell KSM <fashabm@gmail.com>
2022-03-22 10:07:07 +0800

    init

---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <div class="container">
+         <h1>TO DO LIST</h1>
+         <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Item</th>
+      <th scope="col">Status</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Create</td>
+      <td>Not Done</td>
+      <td><button class="btn btn-primary">Done</button></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Read</td>
+      <td>Done</td>
+      <td><button class="btn btn-primary">Not Done</button></td>
+    </tr>
+     <tr>
+      <th scope="row">3</th>
+      <td>Update</td>
+      <td>Done</td>
+      <td><button class="btn btn-primary">Not Done</button></td>
+    </tr>
+     <tr>
+      <th scope="row">4</th>
+      <td>Delete</td>
+      <td>Done</td>
+      <td><button class="btn btn-primary">Not Done</button></td>
+    </tr>
+     </tbody>
+</table>
+  <!-- Content here -->
+</div>
+
+<div class="card">
+  <div class="card-body">
+   <div class="container">
+  <div class="row">
+    <div class="col bg-primary">
+      Ini Col 1
+    </div>
+    <div class="col bg-success">
+      Ini Col 2
+    </div>
+    <div class="col bg-warning">
+      Ini Col 3
+    </div>
+  </div>
+</div>
+  </div>
+</div>
+
+  </body>
+</html>
